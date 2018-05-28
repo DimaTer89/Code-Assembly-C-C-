fcb9c16d775762029a3f362074148731e0d7d833
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-05-28 20:09:15 +0300

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -67,3 +67,69 @@ void main()
 	delete[] arr;
 	system("pause");
 }
+=============================================
+	#include <iostream>
+#include <windows.h>
+using namespace std;
+
+class String
+{
+	char* str;
+public:
+	static int num_obj;
+	String()
+	{
+		cout << " Конструктор по умолчанию \n";
+		str = new char[80];
+		cout << " Введите строку : ";
+		gets_s(str, 80);
+		cout << " Строка : " << str << endl;
+		num_obj++;
+	}
+	String(int n)
+	{
+		cout << " Конструктор с аргументами \n";
+		str = new char[n + 1];
+		cout << " Введите строку : ";
+		while (cin.get() != '\n');
+		gets_s(str, n);
+		cout << " Строка : " << str << endl;
+		num_obj++;
+	}
+	String(char* s)
+	{
+		cout << " Конструктор, который создаёт строку и инициализирует её строкой, полученной от пользователя. \n";
+		str = new char[strlen(s) + 1];
+		strcpy(str, s);
+		cout << " Строка после изменения : " << str << endl;
+		num_obj++;
+	}
+
+	~String()
+	{
+		delete[] str;
+		cout << " Разрушение объекта " << num_obj-- << endl;;
+	}
+
+
+
+};
+int String::num_obj = 0;
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int num;
+	char stroka[256];
+	String str;
+	cout << " Количество объектов : " << String::num_obj << endl;
+	cout << " Введите размер строки : ";
+	cin >> num;
+	String str_1(num);
+	cout << " Количество объектов : " << String::num_obj << endl;
+	cout << " Введите строку : ";
+	gets_s(stroka, 256);
+	String str_2(stroka);
+	cout << " Количество объектов : " << String::num_obj << endl;
+	system("pause");
+}
