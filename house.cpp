2ea30118090ed53865eddc9ac44e5adbb0b9f0d6
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-05-31 20:07:57 +0300

    Update house.cpp

--- house.cpp
+++ house.cpp
@@ -14,8 +14,8 @@ public:
 	}
 	Human(const Human& ob)
 	{
-		 person = ob.person;
-		 profess = ob.profess;
+		person = ob.person;
+		profess = ob.profess;
 	}
 	~Human()
 	{
@@ -33,14 +33,14 @@ public:
 		person = new char[N];
 		profess = new char[N];
 		cout << " Введите ФИО : ";
-		gets_s(person,35);
+		gets_s(person, 35);
 		cout << " Введите профессию : ";
-		gets_s(profess,35);
+		gets_s(profess, 35);
 	}
 	void show()
 	{
-		cout << " ФИО : " <<person << endl;
-		cout << " Профессия : " <<profess << endl;
+		cout << " ФИО : " << person << endl;
+		cout << " Профессия : " << profess << endl;
 	}
 };
 class Flat
@@ -54,6 +54,7 @@ public:
 	Flat()
 	{
 		kol_jil = kol_kom = stage = num_flat = 0;
+		soul = new Human[1];
 	}
 	Flat(const Flat& ob)
 	{
@@ -61,7 +62,7 @@ public:
 	}
 	~Flat()
 	{
-		delete[]soul;
+		delete[] soul;
 		soul = NULL;
 	}
 	void init()
@@ -77,7 +78,7 @@ public:
 		cin >> kol_jil;
 		cin.ignore();
 		soul = new Human[kol_jil];
-		for ( k= 0; k < kol_jil; k++)
+		for (k = 0; k < kol_jil; k++)
 		{
 			cout << " Квартирант № " << k + 1 << endl;
 			soul[k].init();
@@ -104,7 +105,7 @@ public:
 	int num;
 	House()
 	{
-		num = kol_kv = 0; 
+		num = kol_kv = 0;
 		dom = new Flat;
 	}
 	~House()
@@ -137,7 +138,7 @@ public:
 			cout << "\n\n";
 		}
 	}
-	
+
 };
 int main()
 {
@@ -195,120 +196,91 @@ int main()
 			}
 			break;
 		case 3:
-			cout << " 1 - создать новый дом \n";
-			cout << " 2 - изменить квартиру \n";
-			cout << " 3 - добавить квартиру \n";
-			cout << " 4 - удалить квартиру \n";
-			do
+			if (flag == false)
 			{
-				cin >> carte;
-				if (carte < 1 || carte>4)cout << " Ошибка!!! \n";
-			} while (carte < 1 || carte>4);
-			switch (carte)
+				cout << " Создайте сначала класс дом \n";
+				system("pause");
+				break;
+			}
+			else
 			{
-			case 1:
-				if (flag == false)
+				cout << " 1 - создать новый дом \n";
+				cout << " 2 - изменить квартиру \n";
+				do
 				{
-					cout << " Создайте сначала класс дом \n";
-					system("pause");
-					break;
-				}
-				else
+					cin >> carte;
+					if (carte < 1 || carte>4)cout << " Ошибка!!! \n";
+				} while (carte < 1 || carte>4);
+				switch (carte)
 				{
+				case 1:
 					cout << " Введите номер дома : ";
 					cin >> num;
 					cout << " Введите количество квартир : ";
 					cin >> kol;
 					apar.put(num, kol);
 					apar.init();
-					flag = true;
 					break;
-				}
-			case 2:
-				if (flag == false)
-				{
-					cout << " Создайте сначала класс дом \n";
-					system("pause");
-					break;
-				}
-				else
-				{
+				case 2:
 					cout << " Введите номер квартиры для изменения : ";
-					cin >> num_kv;
-					for (int i = 0; i < kol; i++)
-					{
-						if (apar.dom->num_flat == num_kv)apar.dom[i].init();
-					}
-					break;
-				}
-				
-			case 3:
-				int i;
-				Flat* tmp = new Flat[kol];
-					for (i = 0; i<kol; i++)
-					{
-						strcpy(tmp[i].soul->person, apar.dom[i].soul->person);
-						strcpy(tmp[i].soul->profess, apar.dom[i].soul->profess);
-						tmp[i].num_flat = apar.dom[i].num_flat;
-						tmp[i].kol_kom = apar.dom[i].kol_kom;
-						tmp[i].kol_jil = apar.dom[i].kol_jil;
-					}
-					delete[] apar.dom;
-					apar.dom = new Flat[kol + 1];
-					for (i = 0; i < kol; i++)
-					{
-						strcpy(apar.dom[i].soul->person,tmp[i].soul->person);
-						strcpy(apar.dom[i].soul->profess,tmp[i].soul->profess);
-						apar.dom[i].num_flat = tmp[i].num_flat;
-						apar.dom[i].kol_kom = tmp[i].kol_kom;
-						apar.dom[i].kol_jil = tmp[i].kol_jil;
-					}
-					delete[]tmp;
-					apar.dom[kol].init();
-					kol++;
+	         		cin >> num_kv;
+			     	for (int i = 0; i < kol; i++)
+				    {
+					      if (apar.dom->num_flat == num_kv)apar.dom[i].init();
+				    }
 				break;
+			    }
 			}
 			break;
 		case 4:
-			cout << " 1 - поиск по ФИО \n";
-			cout << " 2 - поиск по номеру квартиры \n";
-			do {
-				cin >> x;
-				if (x < 1 || x>2)cout << " Ошибка !!! \n";
-			} while (x < 1 || x>2);
-			switch (x)
+			if (flag == false)
 			{
-			case 1:
-				char name[30];
-				cin.ignore();
-				cout << " Введите фамилию : ";
-				gets_s(name, 30);
-				for (int i = 0; i < kol; i++)
-				{
-					if (strcmp(apar.dom[i].soul->person, name) == 0)
-					apar.dom[i].show();
-				}
-				system("pause");
-				break;
-			case 2:
-				int kv;
-				cout << " Введите номер квартиры : ";
-				cin >> kv;
-				for (int i = 0; i < kol; i++)
-				{
-					if (apar.dom[i].num_flat == kv)
-					apar.dom[i].show();
-				}
+				cout << " Создайте сначала класс дом \n";
 				system("pause");
 				break;
 			}
-			break;
+			else
+			{
+				cout << " 1 - поиск по ФИО \n";
+				cout << " 2 - поиск по номеру квартиры \n";
+				do {
+					cin >> x;
+					if (x < 1 || x>2)cout << " Ошибка !!! \n";
+				} while (x < 1 || x>2);
+				switch (x)
+				{
+				case 1:
+					char name[30];
+					cin.ignore();
+					cout << " Введите фамилию : ";
+					gets_s(name, 30);
+					for (int i = 0; i < kol; i++)
+					{
+						if (strcmp(apar.dom[i].soul->person, name) == 0)
+							apar.dom[i].show();
+					}
+					system("pause");
+					break;
+				case 2:
+					int kv;
+					cout << " Введите номер квартиры : ";
+					cin >> kv;
+					for (int i = 0; i < kol; i++)
+					{
+						if (apar.dom[i].num_flat == kv)
+							apar.dom[i].show();
+					}
+					system("pause");
+					break;
+				}
+				break;
+			}
 		case 0:
 			cout << " До свидания !!! \n";
 			break;
 		default:
-		   cout << " Ошибка !!! \n";
-		   system("pause");
+			cout << " Ошибка !!! \n";
+			system("pause");
 		}
 
 	} while (menu != 0);
