23ad96ee8460a194c9a6ca310122706164ec2c71
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-06-05 18:41:03 +0300

    Update ctroka.cpp

--- ctroka.cpp
+++ ctroka.cpp
@@ -9,49 +9,82 @@ using namespace std;
 class String
 {
 	char* str;
+	char* str1;
+	int len;
 public:
 	String()
 	{
 		str = new char('\0');
+		str1 = new char('\0');
 	}
 	String(const String& ob)
 	{
 		str = new char[strlen(ob.str) + 1];
+		strcpy(str, ob.str);
+		str1 = new char[strlen(ob.str) + 1];
 		strcpy(str, ob.str);
 	}
 	~String()
 	{
 		if (str)delete str;
 		str = NULL;
+		if (str1)delete str;
+		str1 = NULL;
 	}
 	void get(char* s)
 	{
-		str = new char[strlen(s) + 1];
+		len = strlen(s) + 1;
+		str = new char[len];
 		strcpy(str, s);
 	}
 	void show()
 	{
 		cout << " Введённая строка = "<< str << endl;
 	}
-	int operator *(String ob);
+	String& operator *(String& ob);
+	String& operator=(String& ob);
 };
-int String::operator *(String ob)
+String& String::operator *(String& ob)
 {
-	int kol = 0;
-	while (str)
+	int tmp = 1;
+	len = strlen(str) + 1;
+	ob.len = strlen(ob.str) + 1;
+	for (int i = 0; i < len; i++)
 	{
-		char tmp;
-		char tmp2;
-		tmp=*str;
-		tmp2=*ob.str;
-		if (tmp==tmp2)
+		for (int j = 0; j < ob.len; j++)
 		{
-			kol++;
+			if (str[i] == ob.str[j])
+			{
+				tmp++;
+			}
 		}
-		str++;
-		ob.str++;
 	}
-	return kol;
+	str1 = new char[tmp+2];
+	str1[0] = '\0';
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; j < ob.len; j++)
+		{
+			if (str[i] == ob.str[j])
+			{
+				strcat(str1,&str[i]);
+			}
+		}
+	}
+	return *this;
+}
+String& String::operator=(String&ob)
+{
+	if (str)
+		delete[] str;
+	if (str1)
+		delete[] str1;
+	str = new char[strlen(ob.str) + 1];
+	str1 = new char[strlen(ob.str1) + 1];
+	strcpy(str, ob.str);
+	strcpy(str1, ob.str1);
+	len = ob.len;
+	return *this;
 }
 int main()
 {
@@ -69,7 +102,8 @@ int main()
 	gets_s(stroka, 256);
 	ob1.get(stroka);
 	ob1.show();
-	int kol = ob*ob1;
-	cout << " Одинаковых символов = "<< kol << endl;
+	String ob2;
+	ob2 = ob*ob1;
+	ob2.show();
 	system("pause");
 }
