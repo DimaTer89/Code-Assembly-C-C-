b655615ae8ebf5bbcf7d565e95647345d1b3fe76
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-06-08 20:17:46 +0300

    Update viselica.cpp

--- viselica.cpp
+++ viselica.cpp
@@ -58,169 +58,37 @@ public:
 		}
 		fseek(in, 0L, SEEK_SET);
 		slovo* slv = new slovo[kol];
-		char *w, *next;
 		for (int i = 0; i < kol; i++) {
 			fgets(buffer, N, in);
-			w = strtok_s(buffer, "\n", &next);
-			strcpy(slv[i].sl, w);
+			char* tmp;
+			tmp = new char[strlen(buffer) + 1];
+			strcpy(tmp, buffer);
+			strcpy(slv[i].sl, tmp);
 		}
 		int i = rand() % 3;
-		word = new char[strlen(slv[i].sl)];
-		strcpy(word, slv[i].sl);
-	}
-	void play()
-	{
-		char* name = new char[strlen(word) + 1];
-		for (int i = 0; i < strlen(word); i++)
-		{
-			name[i] = '*';
-		}
-		char alfavit[] = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я','\0' };
-		cout << "\n";
-		int len = strlen(word);
-		int kol = 0;
-		int i = 0;
-		while (num > 0 && kol<len)
-		{
-			system("cls");
-			bool flag = false;
-			cout << name << endl;
-			for (int i = 0; i < 32; i++)
-			{
-				cout << " " << alfavit[i];
-			}
-			cout << endl;
-			cout << " Количество попыток = " << num << endl;
-			cout << " Введите букву : ";
-			cin >> sim;
-			for (int i = 0; i < len; i++)
-			{
-				for (int j = 0; j < 33; j++)
-				{
-					if (alfavit[j] == sim)
-					{
-						alfavit[j] = ' ';
-					}
-				}
-				if (word[i] == sim)
-				{
-					kol++;
-					flag = true;
-					name[i] = sim;
-				}
-			}
-			if (flag == false)num--;
-		}
-		if (kol == len)cout << " Вы выиграли \n";
-		if (kol < len)cout << " Вы проиграли \n";
-	}
-};
-int Visel::num = 10;
-int main()
-{
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(0));
-	Visel ob;
-	char key;
-	do 
-	{
-		ob.getWord();
-		ob.play();
-		cout << " Продолжить ? y/n \n";
-		cin >> key;
-
-	} while (key != 'n');
-	system("pause");
-}
-===============================================
-	/*Реализовать виселицу */
-#include <iostream>
-#include <Windows.h>
-#include <time.h>
-#define N 256
-
-using namespace std;
-class Visel
-{
-	char* word;
-	char sim;
-public:
-	static int num;
-	Visel()
-	{
-		word = new char('\0');
-	}
-	Visel(const Visel& ob)
-	{
-		word = new char[strlen(ob.word) + 1];
-		strcpy(word, ob.word);
-	}
-	~Visel()
-	{
-		if (word)
-		{
-			delete word;
-		}
-		word = NULL;
-	}
-	char get(char s)
-	{
-		sim = s;
-		return sim;
-	}
-	void show()
-	{
-		cout << word << endl;
-	}
-	void getWord()
-	{
-		FILE* in;
-		char buffer[N];
-		int kol = 0;
-		struct slovo
-		{
-			char sl[N];
-		};
-		if (fopen_s(&in, "data.txt", "r") != NULL)
-		{
-			cout << " Не удалось открыть файл для чтения \n";
-			system("pause");
-			return;
-		}
-		while (fgets(buffer, N, in) != NULL)
-		{
-			kol++;
-		}
-		fseek(in, 0L, SEEK_SET);
-		slovo* slv = new slovo[kol];
-		for (int i = 0; i < kol; i++) {
-			fgets(buffer, N, in);
-			strcpy(slv[i].sl, buffer);
-		}
-		int i = rand() % 3;
-		word = new char[strlen(slv[i].sl)-1];
+		word = new char[strlen(slv[i].sl)+ 1];
 		strcpy(word, slv[i].sl);
 		delete[]slv;
 	}
 	void play()
 	{
-		char* name = new char[strlen(word) + 1];
-		for (int i = 0; i < strlen(word); i++)
+		char* name = new char[strlen(word)+1];
+		strcpy(name, word);
+		for (int i = 0; i < strlen(name)-1; i++)
 		{
 			name[i] = '*';
 		}
 		char alfavit[] = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я','\0' };
 		cout << "\n";
-		int len = strlen(word);
+		int len = strlen(word)-1;
 		int kol = 0;
-		int i = 0;
-		while (num > 0 && kol<len)
+		int i;
+		while (num > 0&&kol!=len)
 		{
 			system("cls");
 			bool flag = false;
 			cout << name << endl;
-			for (int i = 0; i < 32; i++)
+			for (i = 0; i < 33; i++)
 			{
 				cout << " " << alfavit[i];
 			}
@@ -228,7 +96,8 @@ public:
 			cout << " Количество попыток = " << num << endl;
 			cout << " Введите букву : ";
 			cin >> sim;
-			for (int i = 0; i < len; i++)
+			cin.ignore();
+			for (i = 0; i < len; i++)
 			{
 				for (int j = 0; j < 33; j++)
 				{
@@ -239,18 +108,33 @@ public:
 				}
 				if (word[i] == sim)
 				{
+					name[i] = sim;
 					kol++;
 					flag = true;
-					name[i] = sim;
 				}
 			}
 			if (flag == false)num--;
 		}
-		if (kol == len)cout << " Вы выиграли \n";
-		if (kol < len)cout << " Вы проиграли \n";
+		if (kol == len)
+		{
+			system("cls");
+			cout << word << endl;
+			cout << " Количество попыток = " << num << endl;
+			for (i = 0; i < 33; i++)
+			{
+				cout << " " << alfavit[i];
+			}
+			cout << endl;
+			cout << " Вы выиграли \n";
+		}
+		if (kol < (len-1))
+		{
+			cout << " Вы проиграли \n";
+			cout << " Загаданное слово : " << word << endl;
+		}
 	}
 };
-int Visel::num = 10;
+int Visel::num = 8;
 int main()
 {
 	SetConsoleCP(1251);
@@ -262,131 +146,12 @@ int main()
 	{
 		ob.getWord();
 		ob.play();
-		cout << " Продолжить ? y/n \n";
+		cout << " Продолжить ? д/н \n";
+		cout << " Ваш выбор : ";
 		cin >> key;
-		if (Visel::num == 0)Visel::num = 10;
+		cin.ignore();
+		if (Visel::num == 0)Visel::num = 8;
 
-	} while (key != 'n');
-	system("pause");
-}
-=========================================================
-	/*Реализовать виселицу */
-#include <iostream>
-#include <Windows.h>
-#include <time.h>
-
-using namespace std;
-class Visel
-{
-	char* word;
-	char sim;
-public:
-	static int num;
-	Visel()
-	{
-		word = new char('\0');
-	}
-	Visel(const Visel& ob)
-	{
-		word = new char[strlen(ob.word) + 1];
-		strcpy(word, ob.word);
-	}
-	~Visel()
-	{
-		if (word)
-		{
-			delete word;
-		}
-		word = NULL;
-	}
-	char get(char s)
-	{
-		sim = s;
-		return sim;
-	}
-	void show()
-	{
-		cout << word << endl;
-	}
-	void getWord()
-	{
-		FILE* in;
-		char buffer[256];
-		int kol=0;
-		struct slovo
-		{
-			char sl[64];
-		};
-		if (fopen_s(&in, "data.txt", "r") != NULL)
-		{
-			cout << " Не удалось открыть файл для чтения \n";
-			system("pause");
-			return;
-		}
-		while (fgets(buffer, 256, in) != NULL)
-		{
-			kol++;
-		}
-		fseek(in, 0L, SEEK_SET);
-		slovo* slv = new slovo[kol];
-		char *w, *next;
-		for (int i = 0; i < kol; i++) {
-			fgets(buffer, 256, in);
-			w = strtok_s(buffer, "\n", &next);
-			strcpy(slv[i].sl, w);
-		}
-		int i = rand() % 3;
-		word = new char[strlen(slv[i].sl) + 1];
-		strcpy(word, slv[i].sl);
-	}
-	void play()
-	{
-		char* name = new char[strlen(word) + 1];
-		for (int i = 0; i < strlen(word); i++)
-		{
-			name[i] = '*';
-		}
-		char alfavit[] = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я','\0'};
-		cout << "\n";
-		int len = strlen(word);
-		int kol = 0;
-		int i = 0;
-		while (num > 0&&kol<len)
-		{
-			system("cls");
-			bool flag = false;
-			cout << name << endl;
-			for (int i = 0; i < 32; i++)
-			{
-				cout << " " << alfavit[i];
-			}
-			cout << endl;
-			cout << " Количество попыток = " << num << endl;
-			cout << " Введите букву : ";
-			cin>>sim;
-			for (int i = 0; i < len; i++)
-			{
-				if (word[i]==sim)
-				{
-					kol++;
-					flag = true;
-					name[i] = sim;
-				}
-			}
-			if(flag==false)num--;
-		}
-		if (kol == len)cout << " Вы выиграли \n";
-		if (kol < len)cout << " Вы проиграли \n";
-	}
-};
-int Visel::num = 10;
-int main()
-{
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(0));
-	Visel ob;
-	ob.getWord();
-	ob.play();
+	} while (key != 'н');
 	system("pause");
 }
