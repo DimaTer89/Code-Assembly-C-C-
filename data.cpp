d869b527c862a904c3d033f636c1f86d579674f9
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-06-09 18:32:10 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -192,3 +192,171 @@ int main() {
 	system("pause");
 	return 0;
 }
+=====================================================
+	#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+class date {
+
+	int day;
+	int month;
+	int year;
+
+public:
+	date();
+	date(int day, int month, int year);
+	void set(int day, int month, int year);
+	void show(char symb = '/');
+	int daysAm();
+	void intToDate(int days);
+	int operator- (const date &Obj);
+	date operator+ (const int days);
+	operator int();
+};
+
+int date::operator- (const date &Obj) {
+	date temp = Obj;
+	int res = int(*this) - int(temp);
+	return res;
+}
+date date::operator+ (const int days) {
+	int dayCount = daysAm() + days;
+	intToDate(dayCount);
+	return *this;
+}
+date::operator int() {
+	return daysAm();
+}
+void date::intToDate(int dayCount) {
+	year = 0;
+	int daysinyear = 365;
+	for (dayCount; dayCount>daysinyear; ) {
+		dayCount -= daysinyear;
+		year++;
+		if ((year + 1) % 4 == 0 || ((year + 1) % 100 == 0 && (year + 1) % 400 == 0))daysinyear = 366;
+		else daysinyear = 365;
+	}
+	year++;
+	month = 1;
+	int daysinmonth = 31;
+	while (dayCount>daysinmonth) {
+		dayCount -= daysinmonth;
+		month++;
+		if (month == 1)daysinmonth = 31;
+		else if (month == 2) {
+			if ((year) % 4 == 0 || ((year) % 100 == 0 && (year) % 400 == 0))daysinmonth = 29;
+			else daysinmonth = 28;
+		}
+		else if (month == 3)daysinmonth = 31;
+		else if (month == 4)daysinmonth = 30;
+		else if (month == 5)daysinmonth = 31;
+		else if (month == 6)daysinmonth = 30;
+		else if (month == 7)daysinmonth = 31;
+		else if (month == 8)daysinmonth = 31;
+		else if (month == 9)daysinmonth = 30;
+		else if (month == 10)daysinmonth = 31;
+		else if (month == 11)daysinmonth = 30;
+		else if (month == 12)daysinmonth = 31;
+
+	}
+
+	if (dayCount == 0)day = 1;
+	else day = dayCount;
+}
+int date::daysAm() {
+	bool visokosniy = false;
+	if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))visokosniy = true;
+	int amDays = (year - 1) * 365 + (year - 1) / 4 - ((year - 1) % 400 != 0 && (year - 1) % 100 == 0) + day;
+	for (int i = 2; i <= month; i++) {
+		if (i == 2)amDays += 31;
+		else if (i == 3) {
+			if (visokosniy)amDays += 29;
+			else { amDays += 28; }
+		}
+		else if (i == 4)amDays += 31;
+		else if (i == 5)amDays += 30;
+		else if (i == 6)amDays += 31;
+		else if (i == 7)amDays += 30;
+		else if (i == 8)amDays += 31;
+		else if (i == 9)amDays += 31;
+		else if (i == 10)amDays += 30;
+		else if (i == 11)amDays += 31;
+		else if (i == 12)amDays += 30;
+	}
+	return amDays;
+}
+date::date() {
+	day = 0;
+	month = 0;
+	year = 0;
+}
+date::date(int day, int month, int year) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
+void date::set(int day, int month, int year) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
+void date::show(char symb) {
+	if (day<10) cout << 0;
+	cout << day;
+	cout << symb;
+	if (month<10) cout << 0;
+	cout << month;
+	cout << symb << year << '\n';
+}
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int day1, month1, year1;
+	int day2, month2, year2;
+	date someday;
+	cout << " Введите день, месяц и год через пробел : ";
+	cin >> day1 >> month1 >> year1;
+	someday.set(day1, month1,year1);
+	someday.show();
+	date someDays;
+	cout << " Введите день, месяц и год через пробел : ";
+	cin >> day2 >> month2 >> year2;
+	someDays.set(day2, month2, year2);
+	someDays.show();
+	if (year1 > year2)
+	{
+		int res = someday - someDays;
+		cout << " Между датами " << res << " дня " << endl;
+	}
+	else
+	{
+		int res = someDays - someday;
+		cout << " Между датами " << res << " дня " << endl;
+	}
+	int days;
+	int keys;
+	cout << " К какой дате прибавить дни : \n"
+		<< " 1 - к первой дате \n"
+		<< " 2 - ко второй дате \n";
+	cout << " Ваш выбор : ";
+	cin >> keys;
+	if (keys == 1)
+	{
+		cout << "Сколько прибавить дате : ";
+		cin >> days;
+		someday = someday + days;
+		someday.show('.');
+	}
+	if (keys == 2)
+	{
+		cout << "Сколько прибавить дате : ";
+		cin >> days;
+		someday = someDays + days;
+		someday.show('.');
+	}
+	system("pause");
+	return 0;
+}
