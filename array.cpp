c9a7f46ea18a3d88ca167090e49654e6ed8ce83a
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-07-11 20:32:51 +0300

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -457,22 +457,22 @@ int main()
 #include <time.h>
 
 using namespace std;
-template <class X>
+
 class Array
 {
 
 public:
-	X * arr;
+	int* arr;
 	int size;
 	Array()
 	{
 		size = 0;
-		arr = new X[size];
+		arr = new int[size];
 	}
 	Array(const Array& obj)
 	{
 		size = obj.size;
-		arr = new X[size];
+		arr = new int[size];
 		for (int i = 0; i < size; i++)
 		{
 			arr[i] = obj.arr[i];
@@ -488,17 +488,18 @@ public:
 	void put_rand_arr();
 	void show();
 	void sort();
-	void arr_change(int num);
-	X maxElem();
-	X minElem();
+	void sort1();
+	void arr_change(int num,int i);
+	int maxElem();
+	int minElem();
 };
 
-template <class X> void Array<X>::init(int n)
+void Array::init(int n)
 {
 	size = n;
-	arr = new X[size];
+	arr = new int[size];
 }
-template <class X> void Array<X>::put_arr()
+void Array::put_arr()
 {
 	int i;
 	for (i = 0; i < size; i++)
@@ -508,7 +509,7 @@ template <class X> void Array<X>::put_arr()
 		cin >> arr[i];
 	}
 }
-template <class X> void Array<X>::put_rand_arr()
+void Array::put_rand_arr()
 {
 	int i;
 	for (i = 0; i < size; i++)
@@ -516,7 +517,7 @@ template <class X> void Array<X>::put_rand_arr()
 		arr[i] = rand() % 51 - 50;
 	}
 }
-template <class X> void Array<X>::show()
+void Array::show()
 {
 	int i;
 	cout << " Массив : ";
@@ -526,47 +527,124 @@ template <class X> void Array<X>::show()
 	}
 	cout << endl;
 }
-template <class X> void Array<X>::sort()
+void Array::sort()
 {
 	int i;
 	int k;
+	int* tmp = new int[size];
+	for (int j = 0; j < size; j++)
+	{
+		tmp[j] = arr[j];
+	}
 	for (k = size - 1; k > 0; k--)
 	{
 		for (i = 0; i < k; i++)
 		{
-			if (arr[i] > arr[i + 1])
+			if (tmp[i] > tmp[i + 1])
 			{
-				X tmp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = tmp;
+				int temp = tmp[i];
+				tmp[i] = tmp[i + 1];
+				tmp[i + 1] = temp;
 			}
 		}
 	}
+	cout << " Отсортированный массив \n";
+	for (i = 0; i < size; i++)
+	{
+		cout << tmp[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	delete[]tmp;
 }
-template <class X> void Array<X>::arr_change(int num)
+void Array::sort1()
 {
 	int i;
-	X* arr_new = new X[num];
-	for (i = 0; i < num; i++)
+	int k;
+	int* tmp = new int[size];
+	for (int j = 0; j < size; j++)
 	{
-		arr_new[i] = arr[i];
+		tmp[j] = arr[j];
 	}
-	delete[]arr;
-	arr = arr_new;
-	size = num;
+	for (k = size - 1; k > 0; k--)
+	{
+		for (i = 0; i < k; i++)
+		{
+			if (tmp[i] < tmp[i + 1])
+			{
+				int temp = tmp[i];
+				tmp[i] = tmp[i + 1];
+				tmp[i + 1] = temp;
+			}
+		}
+	}
+	cout << " Отсортированный массив \n";
+	for (i = 0; i < size; i++)
+	{
+		cout << tmp[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	delete[]tmp;
 }
-template <class X> X Array<X>::maxElem()
+void Array::arr_change(int num,int key)
 {
-	X max = arr[0];
+	int i;
+	if (num < size)
+	{
+		int* arr_new = new int[num];
+		for (i = 0; i < num; i++)
+		{
+			arr_new[i] = arr[i];
+		}
+		delete[]arr;
+		arr = arr_new;
+		size = num;
+	}
+	if (num > size&&key==1)
+	{
+		int* arr_new = new int[num];
+		for (i = 0; i < num; i++)
+		{
+			arr_new[i] = arr[i];
+		}
+		for (i = size; i < num; i++)
+		{
+			arr_new[i] = rand() % 51 - 50;
+		}
+		delete[]arr;
+		arr = arr_new;
+		size = num;
+	}
+	else
+	{
+		int* arr_new = new int[num];
+		for (i = 0; i < num; i++)
+		{
+			arr_new[i] = arr[i];
+		}
+		for (i = size; i < num; i++)
+		{
+			cout << " Введите " << i + 1 << " элемент массива : ";
+			cin >> arr_new[i];
+		}
+		delete[]arr;
+		arr = arr_new;
+		size = num;
+	}
+}
+int Array::maxElem()
+{
+	int max = arr[0];
 	for (int i = 0; i < size; i++)
 	{
 		if (arr[i] > max)max = arr[i];
 	}
 	return max;
 }
-template <class X> X Array<X>::minElem()
+int Array::minElem()
 {
-	X min = arr[0];
+	int min = arr[0];
 	for (int i = 0; i < size; i++)
 	{
 		if (arr[i] < min)min = arr[i];
@@ -577,42 +655,184 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	srand(time(0));
-	Array <int> obj;
+	srand((unsigned)time(0));
+	int menu;
 	int size;
+	int mas;
 	int num;
-	cout << " Введите размер массива : ";
-	cin >> size;
-	obj.init(size);
-	obj.put_rand_arr();
-	obj.show();
-	cout << " Введите размер массива : ";
-	cin >> num;
-	obj.arr_change(num);
-	obj.show();
-	cout << " Максимальный элемент массива = " << obj.maxElem() << endl;
-	cout << " Минимальный элемент массива = " << obj.minElem() << endl;
-	obj.sort();
-	obj.show();
-	/*Array <double> obj_1;
-	cout << " Введите размер массива : ";
-	cin >> size;
-	obj_1.init(size);
-	obj_1.put_arr();
-	obj_1.show();
-	cout << " Максимальный элемент массива = " << obj_1.maxElem() << endl;
-	cout << " Минимальный элемент массива = " << obj_1.minElem() << endl;
-	obj_1.sort();
-	obj_1.show();
-	Array <char> obj_2;
-	cout << " Введите размер массива : ";
-	cin >> size;
-	obj_2.init(size);
-	obj_2.put_rand_arr();
-	obj_2.show();
-	cout << " Максимальный элемент массива = " << obj_2.maxElem() << endl;
-	cout << " Минимальный элемент массива = " << obj_2.minElem() << endl;
-	obj_2.sort();
-	obj_2.show();*/
+	int change;
+	int key = 0;
+	Array arr;
+	bool flag = false;
+	do
+	{
+		system("cls");
+		cout << " 1 - заполнить массив \n";
+		cout << " 2 - отображать исходного массива \n";
+		cout << " 3 - изменить размер массива \n";
+		cout << " 4 - сортировать массив по возрастанию \n";
+		cout << " 5 - сортировать массив по убыванию \n";
+		cout << " 6 - определить минимальное и максимальное значение.\n";
+		cout << " 0 - выход \n";
+		cout << " Ваш выбор : ";
+		cin >> menu;
+		switch (menu)
+		{
+		case 1:
+			char key;
+			cout << " Заполнить массив автоматически - y\n";
+			cout << " Заполнить массив собственноручно - n\n";
+			cout << " Ваш выбор : ";
+			cin >> key;
+			if (key == 'y')
+			{
+				cout << " Введите размер массива : ";
+				cin >> size;
+				arr.init(size);
+				arr.put_rand_arr();
+				flag = true;
+				break;
+			}
+			if (key == 'n')
+			{
+				
+					cout << " Введите размер массива : ";
+					cin >> size;
+					arr.init(size);
+					arr.put_arr();
+					flag = true;
+					break;
+				
+			}
+		case 2:
+			if (flag == false)
+			{
+				cout << " Массив не создан \n";
+				system("pause");
+				break;
+			}
+			
+				arr.show();
+				system("pause");
+				break;
+			
+			
+		case 3:
+			if (flag == false)
+			{
+				cout << " Массив не создан \n";
+				system("pause");
+				break;
+			}
+			cout << " Введите размер массива : ";
+			cin >> num;
+			cout << " 1 - Создать новый массив\n";
+			cout << " 2 - Изменения массива с сохранением значений \n ";
+			cout << " Ваш выбор : ";
+			cin >> change;
+			if (change == 1)
+			{
+				char key;
+				cout << " Заполнить массив автоматически - y\n";
+				cout << " Заполнить массив собственноручно - n\n";
+				cout << " Ваш выбор : ";
+				cin >> key;
+				if (key == 'y')
+				{
+					
+						arr.init(num);
+						arr.put_rand_arr();
+						flag = true;
+						break;
+					
+				}
+				if (key == 'n')
+				{
+					
+						arr.init(num);
+						arr.put_arr();
+						flag = true;
+						break;
+					
+				}
+			}
+			if (change == 2)
+			{
+				if (num > arr.size)
+				{
+					cout << " 1 - дополнение массива случайными числами \n";
+					cout << " 2 - дополнение массива вручную \n";
+					cout << " Ваш выбор : ";
+					cin >> key;
+					arr.arr_change(num, key);
+					cout << " Успешно выполнено \n";
+					system("pause");
+					break;
+				}
+				else
+				{
+					arr.arr_change(num, key);
+					cout << " Успешно выполнено \n";
+					system("pause");
+					break;
+				}
+				
+			}
+		case 4:
+			if (flag == false)
+			{
+				cout << " Массив не создан \n";
+				system("pause");
+				break;
+			}
+			
+				arr.sort();
+				break;
+		case 5:
+			if (flag == false)
+			{
+				cout << " Массив не создан \n";
+				system("pause");
+				break;
+			}
+
+			arr.sort1();
+			break;
+		case 6:
+			if (flag == false)
+			{
+				cout << " Массив не создан \n";
+				system("pause");
+				break;
+			}
+			cout << " 1 - максимальное значение массива \n";
+			cout << " 2 - минимальное значение массива \n";
+			cout << " Ваш выбор : ";
+			cin >> mas;
+			if (mas == 1)
+			{
+				
+					cout << " Максимальный элемент массива = " << arr.maxElem() << endl;
+					system("pause");
+					break;
+				
+			}
+			if (mas == 2)
+			{
+				
+					cout << " Минимальный элемент массива = " << arr.minElem() << endl;
+					system("pause");
+					break;
+				
+			}
+		case 0:
+			cout << " До свидания \n";
+			break;
+		default:
+			cout << " Ошибка !!! \n";
+			system("pause");
+		}
+
+	} while (menu != 0);
 	system("pause");
 }
