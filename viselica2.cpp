b259c1d8b386ca2033ba21489f10c055dc6f0aff
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-07-27 20:49:59 +0300

    Update viselica2.cpp

--- viselica2.cpp
+++ viselica2.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <string>
-#include <Windows.h>
+#include <windows.h>
+#include <iostream> 
+#include <string> 
+#include <cstdlib> 
+#include <ctime> 
+#include <cctype> 
 using namespace std;
-
 void paint(int n)
 {
 	if (n == 9)
@@ -56,7 +58,7 @@ void paint(int n)
 	}
 	if (n == 1)
 	{
-		cout << "  ___\n"
+		cout << "   ___\n"
 			<< "  |___|\n"
 			<< "   /|\\\n"
 			<< "    |\n"
@@ -64,7 +66,7 @@ void paint(int n)
 	}
 	if (n == 0)
 	{
-		cout << "   |\n"
+		cout << "    |\n"
 			<< "   _|_\n"
 			<< "  |___|\n"
 			<< "   /|\\\n"
@@ -72,62 +74,81 @@ void paint(int n)
 			<< "   / \\\n ";
 	}
 }
+const int NUM = 26;
+const string wordlist[NUM] = { "apiary","beetle","cereal","danger","ensign",
+"florid","garage","health","insult","jackal",
+"keeper","loaner","manage","nonce","onset",
+"plaid","quilt","remote","stolid","train",
+"useful","valid","whence","xenon","yearn","zippy" };
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int num = 10;
-	cout << endl << endl;
-	num--;
-	paint(num);
-	cout << endl << endl;
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	num--;
-	paint(num);
-	system("pause");
-}
-#include <windows.h>
-#include <iostream>
-#include <string>
-#include <memory>
-
-using namespace std;
-
-int main(int argc, char* argv[])
-{
 	system("cls");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	string str;
-	shared_ptr<string> films[5];
-	for (int i = 0; i < 5; i++)
+
+	srand((unsigned)time(0));
+	char play;
+	cout << "Will you play a word game? <y/n> "; // запуск игры в слова 
+	cin >> play;
+	play = tolower(play);
+	while (play == 'y')
 	{
-		cout << " Введите фильм : ";
-		getline(cin, str);
-		films[i] = shared_ptr<string>(new string(str));
+		system("cls");
+		string target = wordlist[rand() % NUM];
+		int length = target.length();
+		string attempt(length, '-');
+		string badchars;
+		int guesses = 10;
+		cout << "Guess my secret word. It has " << length
+			<< " letters, and you guess\n"
+			<< "one letter at a time. You get " << guesses
+			<< " wrong guesses.\n";
+		cout << "Your word: " << attempt << endl; // вывод слова 
+		while (guesses>0 && attempt != target)
+		{
+			char letter;
+			cout << "Guess a letter: ";
+			cin >> letter;
+			if (badchars.find(letter) != string::npos
+				|| attempt.find(letter) != string::npos)
+			{
+				cout << "You already guessed that. Try again.\n";
+				continue;
+			}
+			int loc = target.find(letter);
+			if (loc == string::npos)
+			{
+				cout << "Oh, bad guess !\n";
+				--guesses;
+				paint(guesses);
+				badchars += letter; // добавить к строке 
+			}
+			else
+			{
+				cout << "Good guess!\n";
+				attempt[loc] = letter;
+				// Проверить, не появляется ли буква еще раз 
+				loc = target.find(letter, loc + 1);
+				while (loc != string::npos)
+				{
+					attempt[loc] = letter;
+					loc = target.find(letter, loc + 1);
+				}
+			}
+			cout << "Your word: " << attempt << endl;
+			if (attempt != target)
+			{
+				if (badchars.length() > 0)
+					cout << "Bad choices: " << badchars << endl;
+				cout << guesses << " bad guesses left\n";
+			}
+		}
+		if (guesses > 0)
+			cout << "That's right!\n";
+		else
+			cout << "Sorry, the word is " << target << " . \n";
+		cout << "Will you play another? <y/n> ";
+		cin >> play;
+		play = tolower(play);
 	}
-	shared_ptr<string> pwin;
-	pwin = films[3];   // films[3] теряет право собственности
-	cout << "Номинант на лучший фильм\n";
-	for (int i = 0; i < 5; i++)
-		cout << *films[i] << endl;
-	cout << "Победитель- " << *pwin << "!\n";
-	system("pause");
+	cout << "Bye\n";
 	return 0;
 }
-
