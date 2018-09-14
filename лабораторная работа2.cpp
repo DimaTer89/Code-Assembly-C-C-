181e7c6a35da15cc6ef09cf6ea6cb38091a24e46
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-09-14 20:03:05 +0300

    Update лабораторная работа2.cpp

--- лабораторная работа2.cpp
+++ лабораторная работа2.cpp
@@ -1,94 +1,48 @@
-#include <iostream>
-#include <windows.h>
-#include <fstream>
+#include <stdio.h>
+#include <Windows.h>
+HANDLE hSemafore_1;
+HANDLE hSemafore_2;
+volatile int* arr;
+int size_1;
 
-
-using namespace std;
-
-struct emp
+DWORD WINAPI MyThreadWork(LPVOID)
 {
-	int num;              // номер зачётки
-	char fam[30];         // фамилия студента
-	char first_name[30];  // имя студента 
-	char second_name[30]; // отчество студента
-	double grade;  	      // средний бал 
-	emp* next;
-};
-
-class Parent
-{
-	emp* head, *tail;
-public:
-	Parent()
+	int sleep;
+	printf(" Введите время сна : ");
+	scanf("%d", &sleep);
+	for (int i = 0; i < size_1; i++)
 	{
-		head = tail = NULL;
+		for (int j = size_1 - 1; j>0 ; j--)
+		{
+			for (int k = j; k < size_1; k++)
+			{
+				if (arr[k] > arr[k + 1])
+				{
+					int tmp = arr[k];
+					arr[k] = arr[k + 1];
+					arr[k + 1] = tmp;
+				}
+			}
+		}
+		printf(" В потоке work подготовлен %d элемент = %d\n", i, arr[i]);
+		ReleaseSemaphore(hSemafore_1, 1, NULL);
+		Sleep(sleep);
 	}
-	~Parent()
-	{
-		deleteList();
-	}
-	void addList();
-	void deleteList();
-	void showList();
-};
-void Parent::addList()
-{
-	int num_1;              // номер зачётки
-	char fam_1[30];         // фамилия студента
-	char first_name_1[30];  // имя студента 
-	char second_name_1[30]; // отчество студента
-	double grade_1;
-	cout << " Введите номер зачётки : ";
-	cin >> num_1;
-	cin.ignore();
-	cout << " Введите фамилию студента : ";
-	gets_s(fam_1, 30);
-	cout << " Введите имя студента : ";
-	gets_s(first_name_1, 30);
-	cout << " Введите отчество студента : ";
-	gets_s(second_name_1, 30);
-	cout << " Введите средний балл : ";
-	cin >> grade_1;
-	emp* elem = new emp;
-	elem->num = num_1;
-	strcpy(elem->fam, fam_1);
-	strcpy(elem->first_name, first_name_1);
-	strcpy(elem->second_name, second_name_1);
-	elem->grade = grade_1;
-	if (head != NULL)
-	{
-		tail->next = elem;
-		tail = elem;
-	}
-	else
-	{
-		head = tail = elem;
-	}
+	return 0;
 }
-void Parent::deleteList()
+
+DWORD WINAPI MySumElement(LPVOID k)
 {
-	emp* tmp = NULL;
-	while (head)
+	int sum = 0;
+	WaitForSingleObject(hSemafore_2, INFINITE);
+	printf(" Поток SumElement начал суммирование элементов массиво до k \n");
+	for (int i = 0; i < (int)k; i++)
 	{
-		tmp = head->next;
-		delete head;
-		head = tmp;
+		sum += arr[i];
 	}
-	tmp = NULL;
-	head = NULL;
-	delete tmp;
-}
-void Parent::showList()
-{
-	while (head)
-	{
-		cout << " Номер зачётки : " << head->num << endl;
-		cout << " Фамилия студента : " << head->fam << endl;
-		cout << " Имя студента : " << head->first_name << endl;
-		cout << " Отчество студента : " << head->second_name << endl;
-		cout << " Средний балл : " << head->grade << endl;
-		head = head->next;
-	}
+	printf(" Сумма элементов массива до k = %d\n", sum);
+	//WaitForSingleObject(hSemafore_2, INFINITE);
+	return 0;
 }
 
 int main()
@@ -96,24 +50,74 @@ int main()
 	system("cls");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	Parent ob;
-	char key = 'д';
-	while (key == 'д')
+	HANDLE hThread_1;
+	HANDLE hThread_2;
+	DWORD IDThread_1;
+	DWORD IDThread_2;
+	int k;
+	printf(" Введите размер массива : ");
+	do
 	{
-		ob.addList();
-		cout << " Добавить студента : д/н ";
-		cin >> key;
-		if (key == 'н')
-			break;
-	}
-	ofstream write_file("D:\\C+\\Parent\\Release\\test.dat",ios::binary);
-	if (!write_file)
+		scanf("%d", &size_1);
+		if (size_1 < 5)printf(" Введите размер не меньше 5 \n");
+	} while (size_1 < 5);
+	arr = new int[size_1];
+	for (int i = 0; i < size_1; i++)
 	{
-		cout << " Не удается открыть файл \n";
-		return 1;
+		printf(" Введите %d элемент : ",i);
+		scanf("%d", &arr[i]);
 	}
-	write_file.write(reinterpret_cast<const char*>(&ob), sizeof ob);
-	write_file.close();
-	system("pause");
+	printf(" Исходный массив \n");
+	for (int i = 0; i < size_1; i++)
+	{
+		printf(" %d  ", arr[i]);
+	}
+	printf("\n");
+	printf(" Введите число k : ");
+	do
+	{
+		scanf("%d", &k);
+		if (k > size_1)printf(" Число k должно быть меньше чем размер массива (%d), введите снова : ", size_1);
+	} while (k > size_1);
+	hSemafore_1 = CreateSemaphore(NULL, 0, size_1, NULL);
+	if (!hSemafore_1)
+	{
+		printf(" Семафор не создан \n");
+		system("pause");
+		return 0;
+	}
+	hSemafore_2 = CreateSemaphore(NULL, 0, k, NULL);
+	if (!hSemafore_2)
+	{
+		printf(" Семафор не создан \n");
+		system("pause");
+		return 0;
+	}
+	hThread_1 = CreateThread(NULL, 0, MyThreadWork, NULL, 0, &IDThread_1);
+	if (!hThread_1)
+	{
+		printf(" Поток не создан \n");
+		system("pause");
+		return 0;
+	}
+	hThread_2 = CreateThread(NULL, 0, MySumElement, (int*)k, 0, &IDThread_2);
+	if (!hThread_2)
+	{
+		printf(" Поток не создан \n");
+		system("pause");
+		return 0;
+	}
+	for (int i = 0; i < size_1; i++)
+	{
+		if (i == (k - 1))
+			ReleaseSemaphore(hSemafore_2, 1, NULL);
+		WaitForSingleObject(hSemafore_1, INFINITE);
+		printf(" Поток main подготовил %d элемент = %d \n", i, arr[i]);
+	}
+	CloseHandle(hThread_1);
+	CloseHandle(hSemafore_1);
+	CloseHandle(hThread_2);
+	CloseHandle(hSemafore_2);
+	delete[] arr;
 	return 0;
 }
