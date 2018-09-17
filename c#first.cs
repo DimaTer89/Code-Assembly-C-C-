7a42997f80494547f7c2be5f39f56e001cb20309
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-09-17 21:25:49 +0300

    Create c#first.cs

---
+++ c#first.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Как вас зовут ?");
+            Console.Write("Введите имя : ");
+            string str=Console.ReadLine();
+            Console.WriteLine("Здравствуйте, "+str);
+            Console.Write("Сколько лет : ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //int age = int.Parse(Console.ReadLine());
+            int age;
+            if(!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine(" Не получилось преобразовать");
+            }
+            else if(age<=18)
+                Console.WriteLine("Привет малолетка");
+            int yearOfBirth = DateTime.Now.Year - age;
+            Console.WriteLine("Вы родились в {0,7} году. Вам {1,4:f3} лет ",yearOfBirth,age);
+            Console.ReadKey();
+        }
+    }
+}
